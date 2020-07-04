70976c3039cf5beee1e9022bcb744c271c1697b0
Lucas Cruz <62730384+lucruzz@users.noreply.github.com>
2020-07-04 12:09:39 -0300

    Configuration files

---
+++ configs/config.py
@@ -0,0 +1,25 @@
+config = Config(
+	app_cache=True,
+	checkpoint_files=None,
+	checkpoint_mode=None,
+	checkpoint_period=None,
+	data_management_max_threads=10,
+	executors=[
+		ThreadPoolExecutor(
+			label='THREADS',
+			managed=True,
+			max_threads=6,
+			storage_access=None,
+			thread_name_prefix='',
+			working_dir=None
+		)
+	],
+	initialize_logging=True,
+	lazy_errors=True,
+	max_idletime=120.0,
+	monitoring=None,
+	retries=0,
+	run_dir='RUNINFO',
+	strategy='simple',
+	usage_tracking=False
+)
